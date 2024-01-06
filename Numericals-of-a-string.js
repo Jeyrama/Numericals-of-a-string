db6bed37052b00555a24ddec58b97a63b2f72189
Jey <126204426+Jeyrama@users.noreply.github.com>
2024-01-06 12:49:38 -0800

    Update Numericals-of-a-string.js

--- Numericals-of-a-string.js
+++ Numericals-of-a-string.js
@@ -15,3 +15,18 @@ Note: There might be some non-ascii characters in the string.
 
 
 // Solution
+
+function numericals(input) {
+  let obj = {};
+  let result = "";
+  for (let i = 0; i < input.length; i += 1) {
+    let current = input[i];
+    if (obj[current]) {
+        obj[current] += 1;
+    } else {
+        obj[current] = 1;
+    }
+    result += obj[current];
+  }
+  return result;
+}
